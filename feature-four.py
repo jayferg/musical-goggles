a51605789b0dc7a04aab032c2f54adc66f3baeb4
jayferg <john@ferguson.net.nz>
2018-08-03 17:52:03 +1200

    Tidy up and refactor

---
+++ feature-four.py
@@ -0,0 +1,5 @@
+# feature-four.py
+
+def feature-four(s: str) -> str:
+	"""return an upper case version of s"""
+	return s.upper()
---
+++ feature-three.py
@@ -0,0 +1,8 @@
+# feature-three.py
+from feature-two import feature-two
+from feature-four import feature-four
+
+def feature-three(s: str) -> str:
+	"""Return an upper case, shuffled version of s"""
+	return feature-four(feature-two(s))
+
