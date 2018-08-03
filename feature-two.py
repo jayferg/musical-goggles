69a0f5d47076cb213b011ee12a2d7e878c8bf7f8
jayferg <john@ferguson.net.nz>
2018-08-03 15:04:20 +1200

    Some documentation tweaks to feature-two

--- feature-two.py
+++ feature-two.py
@@ -2,10 +2,11 @@
 from random import shuffle
 
 def feature-two(s: str) -> str:
-	"""Return a manipulated version of s"""
-	l = list(s)
-	shuffle(l)
+	"""Return a shuffled version of s"""
+	l = list(s)  # We can't shuffle a string, so make a list
+	shuffle(l)   # in place
 	return "".join(l)
+
 
 
 
